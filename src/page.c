f5f92d90247ebb7d0f9732af2344d0a75b090b7b
MarcBrout <marc.brout@epitech.eu>
2017-01-27 17:34:10 +0100

    correction page.c bugs

--- src/page.c
+++ src/page.c
@@ -5,7 +5,7 @@
 ** Login   <duhieu_b@epitech.net>
 ** 
 ** Started on  Fri Jan 27 14:26:01 2017 Benjamin DUHIEU
-** Last update Fri Jan 27 16:31:38 2017 Brout
+** Last update Fri Jan 27 17:33:36 2017 Brout
 */
 
 #include "mymalloc.h"
@@ -40,19 +40,19 @@ t_page *create_big_page(t_page *next, int size, int size_left)
   return (node);
 }
 
-void *set_node_page(size_t size, t_page *node)
+void *set_node_page(size_t size, t_page **node)
 {
   if (size + sizeof(t_page) > getpagesize())
     {
-      if ((node = create_big_page(NULL, size + sizeof(t_page), 0)) == NULL)
+      if ((*node = create_big_page(NULL, size + sizeof(t_page), 0)) == NULL)
 	return NULL;
     }
   else
     {
-      if ((node = create_page(NULL, getpagesize(), getpagesize() - size)) == NULL)
+      if ((*node = create_page(NULL, getpagesize(), getpagesize() - size)) == NULL)
 	return NULL;
     }
-  return ((void *)((uintptr_t)node + sizeof(t_page)));
+  return ((void *)((uintptr_t)(*node) + sizeof(t_page)));
 }
  
 void *add_page(size_t size)
@@ -79,12 +79,16 @@ void *add_page(size_t size)
     {
       if ((pos = add_node(&tmp->root, size, tmp->size)) == NULL)
 	{
-	  pos = set_node_page(size, new_page);
+	  pos = set_node_page(size, &new_page);
 	  tmp->next = new_page;
 	}
-      else
-	return pos;
     }
+  else
+    {
+      pos = set_node_page(size, &new_page);
+      tmp->next = new_page;
+    }
+  return (pos);
 }
 
 void *malloc(size_t size)
@@ -93,7 +97,7 @@ void *malloc(size_t size)
 
   size += size % sizeof(long);
   if (!root)
-    return (set_node_page(size, root));
+    return (set_node_page(size, &root));
   return (add_page(size)); 
 }
 
@@ -107,3 +111,37 @@ void free(void *ptr)
       tmp = tmp->next;
     }
 }
+
+/*
+int main()
+{
+	int *test = malloc(sizeof(int));
+	t_node *node = malloc(sizeof(t_node) * 1000);
+	char *str = malloc(sizeof(char) * 10);
+
+	*test = 10;
+	node->next = NULL;
+	node->size = 10;
+	node->used = false;
+
+	free(node);
+	int *test2 = malloc(sizeof(int));
+	*test2 = 13;
+	str[0] = '0';
+	str[1] = '1';
+	str[2] = '2';
+	str[3] = '3';
+	str[4] = '4';
+	str[5] = '5';
+	str[6] = '6';
+	str[7] = '7';
+	str[8] = '8';
+	str[9] = '9';
+	int *test3 = malloc(sizeof(int));
+
+	free(test);
+	free(test2);
+	free(str);
+	return (0);
+}
+*/
