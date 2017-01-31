5fc15016fe81dcab6e918670afb971d35cbd2116
Benjamin Duhieu <benjamin.duhieu@epitech.eu>
2017-01-31 15:16:24 +0100

    Debugging free...

--- src/myfree.c
+++ src/myfree.c
@@ -5,7 +5,7 @@
 ** Login   <duhieu_b@epitech.net>
 ** 
 ** Started on  Fri Jan 27 18:02:44 2017 Benjamin DUHIEU
-** Last update Tue Jan 31 11:05:35 2017 marc brout
+** Last update Tue Jan 31 12:33:16 2017 Benjamin DUHIEU
 */
 
 #include <unistd.h>
@@ -28,7 +28,7 @@ static void	node_fuse(t_page *start, t_node *cur)
 {
   cur->used = false;
   start->size_left += cur->size;
-  if (cur->prev && !cur->prev->used)
+  /*  if (cur->prev && !cur->prev->used)
     {
       cur->prev->size += cur->size + sizeof(t_node);
       cur->prev->next = cur->next;
@@ -44,7 +44,7 @@ static void	node_fuse(t_page *start, t_node *cur)
       if (cur->next)
 	  cur->next->prev = cur;
       start->size_left += sizeof(t_node);
-    }
+      }*/
 }
 
 static bool	free_node(t_page *start, void *ptr)
@@ -77,12 +77,11 @@ void		free(void *ptr)
       prev = tmp;
       tmp = tmp->next;
     }
-  /*  if (!tmp->next && page_is_free(tmp))
+  if (tmp && !tmp->next && page_is_free(tmp))
     {
       if (prev)
 	prev->next = NULL;
-      sbrk(-tmp->size);
+      sbrk(tmp->size * (-1));
     }
-  */
   pthread_mutex_unlock(&mutex);
 }
